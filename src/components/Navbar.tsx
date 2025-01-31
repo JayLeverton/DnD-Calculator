74a8d7e5d38a1388a995bb0fb3aa7dca354989cf
Jay Leverton <leverton.web@gmail.com>
2025-01-31 16:29:48 +0000

    title, bg color, and navbar color change

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <div className="flex w-full fixed top-0 left-0 border-b items-center h-[--navbar-height]">
+    <div className="flex w-full fixed top-0 left-0 border-b items-center h-[--navbar-height] bg-[#121212]">
       <div className="flex flex-col border-r h-full justify-center px-6">
         <Link className="" to="DnD-Calculator/">
           Home
